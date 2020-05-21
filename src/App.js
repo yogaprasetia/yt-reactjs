2f23c4e1aa60f177d49cef0286d382c03a83f340
yogaprasetia <yogapzx@gmail.com>
2020-05-21 11:46:50 +0700

    commit kedua

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import { Grid } from '@material-ui/core';
+
+import { SearchBar } from './components/SearchBar';
+import { VideoList } from './components/VideoList';
+import { VideoDetail } from './components/VideoDetail';
+
+import youtube from './api/youtube';
+
 class App extends React.Component{
     render (){
         return(
-            <h1>Youtube Clone App</h1>
+            <Grid justify="center" container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            {/* {SEARCH BAR} */}
+                        </Grid>
+                        <Grid item xs={8}>
+                            {/* {VIDEO DETAIL} */}
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* {VIDEO LIST} */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
