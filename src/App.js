6a6106c620f4b75c983530a128337c9b47781919
yogaprasetia <yogapzx@gmail.com>
2020-05-21 15:18:09 +0700

    commit ketiga

--- src/App.js
+++ src/App.js
@@ -2,26 +2,51 @@ import React from 'react';
 
 import { Grid } from '@material-ui/core';
 
-import { SearchBar } from './components/SearchBar';
-import { VideoList } from './components/VideoList';
-import { VideoDetail } from './components/VideoDetail';
+import { SearchBar, VideoDetail, VideoList } from './components';
 
 import youtube from './api/youtube';
 
 class App extends React.Component{
+    state = {
+        videos: [],
+        selectedVideo: null,
+    }
+
+    componentDidMount(){
+        this.handleSubmit('last child ketidakrelaanku')
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    }
+
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', { 
+            params: {
+            part: 'snippet',
+            maxResult: 5,
+            key: 'API_KEY',
+            q: searchTerm,
+            }
+        });
+        
+        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+    }
+
     render (){
+        const { selectedVideo, videos } = this.state;
         return(
-            <Grid justify="center" container spacing={16}>
+            <Grid justify="center" container spacing={10}>
                 <Grid item xs={12}>
                     <Grid container spacing={16}>
                         <Grid item xs={12}>
-                            {/* {SEARCH BAR} */}
+                            <SearchBar onFormSubmit={this.handleSubmit} />
                         </Grid>
                         <Grid item xs={8}>
-                            {/* {VIDEO DETAIL} */}
+                            <VideoDetail video={selectedVideo}/>
                         </Grid>
                         <Grid item xs={4}>
-                            {/* {VIDEO LIST} */}
+                            <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
                         </Grid>
                     </Grid>
                 </Grid>
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,9 +2,4 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResult: 5,
-        key: '[API KEY]'
-    }
 });
